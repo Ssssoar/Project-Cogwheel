3c1fb530106effde92bc772061aa0c17604e8708
Ssssoar <jano20@vtr.net>
2024-07-26 16:16:45 -0400

    Fixed the Sound Clipping issue

--- Assets/Scripts/SoundFXManager.cs
+++ Assets/Scripts/SoundFXManager.cs
@@ -6,8 +6,11 @@ public class SoundFXManager : MonoBehaviour
 {
     AudioSource audioSource;
     public static SoundFXManager instance;
+    public int maxSimultaneous;
     AudioSource toStop;
     float stopTimer = -1f;
+    Dictionary<AudioClip,int> playingClips;
+    Dictionary<LibreriaDeSonidos,int> playingLibs;
 
     private void Awake()
     {
@@ -18,12 +21,28 @@ public class SoundFXManager : MonoBehaviour
     }
     public void ReproducirSFX(LibreriaDeSonidos lib)
     {
-        audioSource.PlayOneShot(lib.clip);
+        if (playingLibs == null){
+            playingLibs = new Dictionary<LibreriaDeSonidos,int>();
+        }
+        if(!playingLibs.ContainsKey(lib))
+            playingLibs.Add(lib,1);
+        if (playingLibs[lib] <= maxSimultaneous){
+            playingLibs[lib]++;
+            audioSource.PlayOneShot(lib.clip);
+        }
     }
 
     public void ReproducirSFX(AudioClip clip)
     {
-        audioSource.PlayOneShot(clip);
+        if (playingClips == null){
+            playingClips = new Dictionary<AudioClip,int>();
+        }
+        if(!playingClips.ContainsKey(clip))
+            playingClips.Add(clip,1);
+        if (playingClips[clip] <= maxSimultaneous){
+            playingClips[clip]++;
+            audioSource.PlayOneShot(clip);
+        }
     }
 
     public void ReproducirDetenible(AudioClip clip, float timer){
@@ -37,6 +56,8 @@ public class SoundFXManager : MonoBehaviour
     }
 
     void Update(){
+        playingClips = null;
+        playingLibs = null;
         if (stopTimer == -1f) return;
         stopTimer -= Time.deltaTime;
         if (stopTimer <= 0){
